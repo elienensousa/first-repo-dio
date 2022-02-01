56e863c45b75dd909ad204be49c1b07dd06a4c71
Eliene Sousa <eliene.nunes1@hotmail.com>
2022-02-01 01:54:54 -0300

    Inclusao de exercicios realizados durante o curso

---
+++ js_catAPI/assets/js/scripts.js
@@ -0,0 +1,37 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/'
+const catBtn = document.getElementById('change-cat')
+const catImg = document.getElementById('cat')
+
+
+const getCats = async () => {
+    try{
+        const data = await fetch(BASE_URL)
+        const json = await data.json()
+
+        return json.webpurl
+    }catch(e){
+        console.log(e.message)
+    }
+}
+
+
+const loadImg = async() => {
+    
+    catImg.src = await getCats()
+}
+
+catBtn.addEventListener('click', loadImg)
+
+
+loadImg()
+
+/*
+//Outra forma sem try..catch
+const getCats = async () => {
+    
+        const data = await fetch(BASE_URL)
+        .then(res => res.json())
+        .catch(e => console.log(e))
+        
+        return data.webpurl
+}*/
